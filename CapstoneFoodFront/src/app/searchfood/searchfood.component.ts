59d33a7e992fd823306427ed27171d74c3a1bca5
naderbenkhalifa <nader.benkhalifa78@gmail.com>
2023-02-23 15:09:54 +0100

    second commit

---
+++ CapstoneFoodFront/src/app/searchfood/searchfood.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Food } from 'src/capstoneFood-api/src/models';
+import { FoodsService } from 'src/capstoneFood-api/src/services';
+
+@Component({
+  selector: 'app-searchfood',
+  templateUrl: './searchfood.component.html',
+  styleUrls: ['./searchfood.component.scss']
+})
+export class SearchfoodComponent implements OnInit {
+  
+  food:any;
+  id:any
+  constructor(private router:Router) { }
+  
+  public getFoodById(id:number){
+    console.log("getFoodById: id="+id);
+    this.router.navigate(['/foodFound',id]);
+  }
+
+  ngOnInit(): void {
+  }
+  
+
+}
